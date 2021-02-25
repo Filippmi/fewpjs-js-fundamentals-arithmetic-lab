ad35f4c931763313a53bd9c1c9f4a2782b5a9eaf
Filipp Minayev <Filippmi@gmail.com>
2021-02-25 14:13:39 -0700

    commit tree 8e080fce89a771647cdb257a7a6eefa473bcbb1e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 16
+let num4 = 6
+let numbers = (1, 2, 3, 5, 20)
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(numbers)
